a4d8150f4bef500bddd14b281c5a9d040b1fe152
Anjher Gustavo <31428665+anjhergustavo@users.noreply.github.com>
2023-10-06 22:39:36 -0500

    Delete Angulos Alex y Jesus directory

--- Angulos Alex y Jesus/testimage_anjher.m
+++
@@ -1,6 +0,0 @@
-N = 300;
-[x,y] = meshgrid(linspace(-1,1,N));
-testimage = cos(2*pi*(10*y));
-figure(1), imagesc(testimage), truesize;
-TESTIMAGE = fftshift(fft2(testimage));
-figure(2), imagesc(abs(TESTIMAGE)), truesize
